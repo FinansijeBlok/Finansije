2c9884a52113a158771deea6f2ec4114c0773e89
Veljko <veljkostojanovicima@gmail.com>
2024-05-23 11:37:18 +0200

    Save

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
 
-function App() {
+import { Transaction } from "./transaction";
+import { transformSync } from "@babel/core";
+
+
+const App: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState <number | string>('');
+  const [category, setCategory] = useState<string>('');
+
+  const addTransaction = () => {
+
+    if(description && amount){
+
+        const newTransaction : Transaction = {
+          id: transactions.length ? transactions[transactions.length - 1].id + 1 : 1,
+          description,
+          amount: Number(amount),
+          category,
+      };
+
+      setTransactions([...transactions, newTransaction]);
+      setDescription('');
+      setAmount('');
+      setCategory('');
+    }
+
+
+
+  };
+
+  const deleteTransaction = (id: number) => {
+    setTransactions(transactions.filter(trans => trans.id !== id));
+  }
+
+  const calculateTotal = () => {
+    return transactions.reduce((total, transaction) => total + transaction.amount, 0);
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Transaction Tracker</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <input
+        type = "text"
+        placeholder = "Category"
+        value = {category}
+        onChange={(e) => setCategory(e.target.value)}
+        />
+
+        <button onClick={addTransaction}>Add Transaction</button>
+      </div>
+      <h2>Transactions</h2>
+      <ul>
+        {transactions.map((transaction) => (
+          <li key={transaction.id}>
+            {transaction.description}: ${transaction.amount}
+            <button onClick={() => deleteTransaction(transaction.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <h2>Total: ${calculateTotal()}</h2>
     </div>
   );
-}
+};
 
 export default App;
